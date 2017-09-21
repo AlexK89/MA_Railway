2e12d4bcdaae1007f1f35a3c0a9b2ab9c055d98a
Vasyl Kozhushko <kozhushko.vasyl@gmail.com>
2017-09-21 20:48:28 +0100

    railroad constraction calculator

--- railway_calc.php
+++ railway_calc.php
@@ -1,51 +1,3 @@
-<?php
-ini_set('display_errors', 'On');
-error_reporting(E_ALL | E_STRICT);
-
-    if(isset($_POST["submit"])) {
-        $length = $_POST["length"] * 1;
-        $posts = $_POST["posts"] * 1;
-        $railings = $_POST["railings"] * 1;
-        result($length, $posts, $railings);
-    }
-    function result($length, $posts, $railings) {
-           if ($_POST["posts"] < 2 && $_POST["posts"] < 2 && $_POST["length"] === "") {
-                echo "Sorry, lack of components";
-            } else {
-               components_calc($length);
-               length($posts, $railings);
-            }
-    }
-    function components_calc($length)
-    {
-        $posts_amount = 1;
-        $railings_amount = 0;
-
-        if ($length == 0) {
-            $posts_amount = 0;
-        }
-        while($length > 0 ) {
-            $posts_amount++;
-            $railings_amount++;
-            $length -= 1.7;
-        }
-        echo "<p>Posts: " . $posts_amount . "</p>";
-        echo "<p>Railings: " . $railings_amount . "</p>";
-    }
-    function length($posts, $railings)
-    {
-        $max_length = 0;
-        if ($posts > 1 && $railings > 0) {
-            $max_length = 0.1;
-        }
-        while ($posts > 1 && $railings > 0) {
-            $max_length += 1.6;
-            $posts--;
-            $railings--;
-        }
-        echo "<p>Length: " . $max_length . "</p>";
-    }
-?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -58,17 +10,23 @@ error_reporting(E_ALL | E_STRICT);
 <body>
 <form action="railway_calc.php" method="post">
     <h4>What length do you need?</h4>
-    <p><input type="number" placeholder="Length" name="length" step=".1"></p>
-    <p><input type="submit" name="submit"></p>
-    <p>Available Railings</p>
-    <p>===============================</p>
-    <p>OR</p>
-    <p>===============================</p>
-    <h4>Provide available amount of components</h4>
-    <p><input type="number" placeholder="Amount of railings" name="railings""></p>
-    <p>Available Posts</p>
-    <p><input type="number" placeholder="Amount of posts" name="posts""></p>
-    <p><input type="submit" name="submit"></p>
+    <p><input type="number" pattern="[0-9]" placeholder="Length" name="length" step=".1"></p>
+    <p><input type="submit" name="submit_components_calc"></p>
 </form>
+<?php
+    require ("components_calc.php");
+?>
+<form action="railway_calc.php" method="post">
+    <h4>Available Railings</h4>
+    <p><input type="number" pattern="[0-9]" placeholder="Amount of railings" name="railings""></p>
+    <h4>Available Posts</h4>
+    <p><input type="number" pattern="[0-9]" placeholder="Amount of posts" name="posts""></p>
+    <p><input type="submit" name="submit_length_calc"></p>
+</form>
+<p class="result">
+    <?php
+        require ("length_calc.php");
+    ?>
+</p>
 </body>
 </html>
